2293c4f04bcd03e6629bc5a9308b00defc801766
Mohit <mohitmadan71@gmail.com>
2017-10-24 21:31:37 +0530

    alu labels changed

--- Components/alu.vhd
+++ Components/alu.vhd
@@ -3,26 +3,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
  
 entity alu is
- Port ( inp_a : in signed(16 downto 0);
+ Port ( reg_a : in signed(16 downto 0);
  inp_b : in signed(16 downto 0);
- sel : in STD_LOGIC_VECTOR (1 downto 0);
- out_alu : out signed(16 downto 0));
+ op_sel : in STD_LOGIC_VECTOR (1 downto 0);
+ reg_c : out signed(16 downto 0));
 end alu;
  
 architecture Behavioral of alu is
 begin
 
-process(inp_a, inp_b, sel) 
+process(reg_a, inp_b, op_sel) 
 begin
-case sel is
+case op_sel is
 	when "00" => 
-		out_alu<= inp_a + inp_b; --addition 
+		reg_c<= reg_a + inp_b; --addition 
 	when "01" => 
-		out_alu<= inp_a - inp_b; --subtraction 
+		reg_c<= reg_a - inp_b; --subtraction 
 	when "10" => 
-		out_alu<= inp_a + 1; --add 1 
+		reg_c<= reg_a + 1; --add 1 
 	when "11" => 
-		out_alu<= inp_a nand inp_b; --nand  
+		reg_c<= reg_a nand inp_b; --nand  
 
 	when others =>
 	NULL;
