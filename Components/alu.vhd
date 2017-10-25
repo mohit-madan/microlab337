dbdd0066c57b3a692fe206a8c15d423e00c2f396
Mohit <mohitmadan71@gmail.com>
2017-10-25 21:33:41 +0530

    priority encoder added

--- Components/alu.vhd
+++ Components/alu.vhd
@@ -8,9 +8,10 @@ entity alu is
  en_c : in STD_LOGIC; --enable for carry flag
  en_z : in STD_LOGIC; -- enable for zero flag
  op_sel : in STD_LOGIC_VECTOR (2 downto 0);
- reg_c : out signed(15 downto 0));
+ reg_c : out signed(15 downto 0);
  c : out STD_LOGIC;
- z : out STD_LOGIC;
+ z : out STD_LOGIC
+);
 end alu;
 
  
@@ -23,33 +24,40 @@ case op_sel is
 	when "000" => 
 		reg_c <= reg_a + reg_b; --addition 
 		-- flag modification
-		if (reg_a + reg_b > "0111111111111111") and (en_c ="1") then 
-			c = "1";
-		if(reg_a + reg_b = 0) and (en_z = "1") then
-			z = "1";
+		if (reg_a + reg_b > "0111111111111111") and (en_c ='1') then 
+			c <= '1';
+		end if;
+
+		if(reg_a + reg_b = 0) and (en_z = '1') then
+			z <= '1';
+		end if;
 			
 	when "001" => 
 		reg_c<= reg_a - reg_b; --subtraction
 		-- flag modification
-		if(reg_a = reg_b) and (en_z = "1") then
-			z = "1";
-		if(reg_a < reg_b) and (en_c ="1") then
-			c = "1";	
+		if(reg_a = reg_b) and (en_z = '1') then
+			z <= '1';
+		end if;
+		if(reg_a < reg_b) and (en_c ='1') then
+			c <= '1';	
+		end if;
 	
 	when "010" => 
-		reg_c<= reg_a + "1"; --add 1 
+		reg_c<= reg_a + 1; --add 1 
 	
 	when "011" => 
 		reg_c<= reg_a nand reg_b; --nand
 		-- flag modification
-		if(reg_a = reg_b) and (en_z = "1") then
-			z = "1";  
+		if(reg_a = reg_b) and (en_z = '1') then
+			z <= '1';  
+		end if;
 
 	when "100" =>
 		reg_c<= reg_a xor reg_b; --xor
 		-- flag modification
-		if(reg_a = reg_b) and (en_z = "1") then
-			z = "1";		
+		if(reg_a = reg_b) and (en_z = '1') then
+			z <= '1';		
+		end if;
 
 	when others =>
 	NULL;
