3113d46752b0a416856258f6faf61926bf867081
Anmol Gupta <sam.amlgpt@gmail.com>
2017-10-25 20:38:03 +0530

    Made slight modifications for specifications

--- Components/regfile.vhd
+++ Components/regfile.vhd
@@ -5,19 +5,19 @@ use IEEE.NUMERIC_STD.ALL;
 entity register_file is   
     port
     (
-    outA          : out std_logic_vector(15 downto 0);
-    outB          : out std_logic_vector(15 downto 0);
-    input         : in  std_logic_vector(15 downto 0);
+    rf_d1          : out std_logic_vector(15 downto 0);
+    rf_d2          : out std_logic_vector(15 downto 0);
+    rf_d3          : in  std_logic_vector(15 downto 0);
     writeEnable   : in std_logic;
-    regASel       : in std_logic_vector(3 downto 0);
-    regBSel       : in std_logic_vector(3 downto 0);
-    writeRegSel   : in std_logic_vector(3 downto 0);
+    regASel       : in std_logic_vector(2 downto 0);
+    regBSel       : in std_logic_vector(2 downto 0);
+    rf_a3   : in std_logic_vector(3 downto 0);
     clk           : in std_logic
     );
 end register_file;
 
 architecture behavioral of register_file is
-type registerFile is array(0 to 15) of std_logic_vector(15 downto 0);
+type registerFile is array(0 to 7) of std_logic_vector(15 downto 0); --Eight 16-bit registers
 signal registers : registerFile;
 begin
 
@@ -25,7 +25,7 @@ begin
     begin
         if rising_edge(clk) then 
             if(writeEnable = '1') then
-                registers(to_integer(unsigned(writeRegSel))) <= input;
+                registers(to_integer(unsigned(writeRegSel))) <= input1;
             end if;
             if falling_edge(clk) then
                 outA <= registers(to_integer(unsigned(regASel)));
